cbc53d8cf8e639067533a795510ca3dad640514f
imirceagit <mircea.ionita@hotmail.com>
2016-11-09 04:19:54 +0200

    weather app

--- InstaSlam/app/src/main/java/com/mient/instaslam/instaslam/activities/MediaActivity.java
+++ InstaSlam/app/src/main/java/com/mient/instaslam/instaslam/activities/MediaActivity.java
@@ -336,22 +336,46 @@ public class MediaActivity extends AppCompatActivity {
         }
     }
 
-    public Bitmap decodeURI(String filePath){
+    public Bitmap decodeURI(String filePath) {
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(filePath,options);
+        BitmapFactory.decodeFile(filePath, options);
 
-        //TODO SCALING IMAGES FOR THUMBNAILS
-        Boolean scaleByHeight = Math.abs(options.outHeight - 100) >- Math.abs(options.outWidth - 100);
+        // Only scale if we need to
+        // (16384 buffer for img processing)
+        Boolean scaleByHeight = Math.abs(options.outHeight - 100) >= Math.abs(options.outWidth - 100);
         if(options.outHeight * options.outWidth * 2 >= 16384){
-            double sampleSize = scaleByHeight ? options.outHeight / 1000 : options.outWidth / 100;
-            options.inSampleSize = (int)Math.pow(2d, Math.floor(Math.log(sampleSize)/Math.log(2d)));
+            // Load, scaling to smallest power of 2 that'll get it <= desired dimensions
+            double sampleSize = scaleByHeight
+                    ? options.outHeight / 1000
+                    : options.outWidth / 1000;
+            options.inSampleSize =
+                    (int)Math.pow(2d, Math.floor(
+                            Math.log(sampleSize)/Math.log(2d)));
         }
 
         options.inJustDecodeBounds = false;
         options.inTempStorage = new byte[512];
-
         Bitmap output = BitmapFactory.decodeFile(filePath, options);
         return output;
     }
+
+//    public Bitmap decodeURI(String filePath){
+//        BitmapFactory.Options options = new BitmapFactory.Options();
+//        options.inJustDecodeBounds = true;
+//        BitmapFactory.decodeFile(filePath,options);
+//
+//        //TODO SCALING IMAGES FOR THUMBNAILS
+//        Boolean scaleByHeight = Math.abs(options.outHeight - 100) >- Math.abs(options.outWidth - 100);
+//        if(options.outHeight * options.outWidth * 2 >= 16384){
+//            double sampleSize = scaleByHeight ? options.outHeight / 1000 : options.outWidth / 100;
+//            options.inSampleSize = (int)Math.pow(2d, Math.floor(Math.log(sampleSize)/Math.log(2d)));
+//        }
+//
+//        options.inJustDecodeBounds = false;
+//        options.inTempStorage = new byte[512];
+//
+//        Bitmap output = BitmapFactory.decodeFile(filePath, options);
+//        return output;
+//    }
 }
